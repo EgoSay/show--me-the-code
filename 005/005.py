7e9091e7317d0c40d2b6e3d18a7f0e1ce17c2ffa
low-key-Tony <961663316@qq.com>
2019-04-20 23:03:02 +0800

    005：调整分辨率

---
+++ 005/005.py
@@ -0,0 +1,26 @@
+# @Author  : codeAC
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/3/10 21:15
+# @IDE     : PyCharm
+from PIL import Image
+import glob
+import os
+
+
+def resize():
+    for files in glob.glob('*.jpg'):
+        filepath, filename = os.path.split(files)  # 分割文件名和路径名
+        fname, fext = os.path.splitext(filename)
+        im = Image.open(files)
+        w, h = im.size
+        if w > 640:
+            x = w / 640.0
+            w = 640
+            h = int(h / x)
+        if h > 1136:
+            x = h / 1136.0
+            h = 1136
+            w = int(w / x)
+        print(w, h)
+        im0 = im.resize((w, h), Image.ANTIALIAS)
+        im0.save('0005' + filename)
